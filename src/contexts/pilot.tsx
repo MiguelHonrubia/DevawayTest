0d856663f9c137c594a341a01b82feede4fdf80b
miguelhonrubia <almihonrubia@gmail.com>
2021-05-08 00:47:22 +0200

    added pilot context

---
+++ src/contexts/pilot.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { PilotType } from "../models/pilot";
+import { getPilots, getPilotById } from "../services/pilot";
+
+interface PilotState {
+  loading: boolean;
+  pilots: PilotType[];
+  selectedPilot: PilotType;
+}
+
+interface PilotStateContext extends PilotState {
+  setState?: React.Dispatch<React.SetStateAction<PilotState>>;
+}
+
+const initialPilotState: PilotState = {
+  loading: false,
+  pilots: [],
+  selectedPilot: {} as PilotType,
+};
+
+const initialPilotStateContext: PilotStateContext = {
+  ...initialPilotState,
+  setState: undefined,
+};
+
+export const PilotContext = React.createContext<PilotStateContext>(
+  initialPilotStateContext
+);
+
+export const usePilotContext = () => {
+  const context = React.useContext(PilotContext);
+
+  const setLoadingState = () => {
+    context.setState &&
+      context.setState((state) => ({
+        ...state,
+        loading: true,
+      }));
+  };
+
+  const fetchAllPilots = async () => {
+    setLoadingState();
+
+    const pilotsAux = await getPilots();
+
+    context.setState &&
+      context.setState((state) => ({
+        ...state,
+        pilots: pilotsAux,
+        loading: false,
+      }));
+  };
+
+  const fetchPilot = async (id: string) => {
+    setLoadingState();
+
+    const pilot = await getPilotById(id);
+
+    context.setState &&
+      context.setState((state) => ({
+        ...state,
+        selectedPilot: pilot,
+        loading: false,
+      }));
+  };
+
+  return { context };
+};
+
+export const PilotProvider = ({ children }: any) => {
+  const [state, setState] = React.useState(initialPilotState);
+  return (
+    <PilotContext.Provider
+      value={{
+        loading: state.loading,
+        pilots: state.pilots,
+        selectedPilot: state.selectedPilot,
+      }}
+    >
+      {children}
+    </PilotContext.Provider>
+  );
+};
